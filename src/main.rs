ec5723a3143e8b60868d6018b3093dc59ff74ad7
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-10 21:29:28 +0330

    12. Compiler Directives 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 type Meters =  i32 ;
 
-// #[allow(unused_variables)]
+#[allow(unused_variables)]
 fn main() {
-    #[allow(unused_variables)]
 let  mile_race_length: Meters = 1600;
-    #[allow(unused_variables)]
 let  two_mile_race_length: Meters = 3200;
 
 
