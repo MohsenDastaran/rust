fe4d4d05ae81c75dda695ebb4f6e8d681696a2d6
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-10 21:30:27 +0330

    12. Compiler Directives  global file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+#![allow(unused_variables)]
+
 type Meters =  i32 ;
 
-#[allow(unused_variables)]
 fn main() {
 let  mile_race_length: Meters = 1600;
 let  two_mile_race_length: Meters = 3200;
-
-
 
 }
