78232893fbf5c2bb100be8d73fd08baf201c30b8
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-11 02:38:18 +0330

    6. Strings and Raw Strings

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
-// #![allow(unused)]
 
-const TOUCHDOWN_POINTS:i32 = 6;
 fn main() {
-let season: &str  = "Fall";
-let mut  points_scored: i32 = 28;
-points_scored = 28;
 
-
-
-let  event_time: &str = "06:00";
-let  event_time: i32 = 6;
-
-
-
-println!("my season {TOUCHDOWN_POINTS}, {season}, {points_scored} , {points_scored}, {event_time}")}
+println!("Hello world, \nHow are you?");
+println!("\tHello world, \"How are you?\"");
+let filepath = r"C:\files";
+println!("{filepath}")
+}
