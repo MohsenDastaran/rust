83aecbd9500c7349483ea371248044a36d23e400
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-10 19:06:54 +0330

    variabl shadowing demonstration

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,15 @@
-#[derive(Debug)]
-struct Deck {
-    cards: Vec<String>
-}
-impl Deck  {
-    fn new() -> Self {
-    // list of cards in a deck
-
-    let mut cards: Vec<String> =  vec![];
-    let suits: [&str; 4] = ["Hearts", "Diamonds", "Clubs", "Spades"];
-    let values: [&str; 13] =["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
-
-    for suit in suits {
-        for value in values { 
-            let card: String = format!("{value} of {suit}"); 
-            cards.push(card);
-        }
-
-    }
-
-    let deck = Deck { cards };
-    return deck;
-    }
-}
 
 fn main() {
-  let deck = Deck::new();
-    println!("Here is ur Deck: {:#?}", deck);
+  let grams_of_protein = "100.25";
+  println!("grams_of_protein: {}", grams_of_protein);
+  let grams_of_protein = 100.25;
+  println!("grams_of_protein: {}", grams_of_protein);
+  let mut grams_of_protein = 100;
+  println!("grams_of_protein: {}", grams_of_protein);
+
+  grams_of_protein = 105;
+  println!("grams_of_protein: {}", grams_of_protein);
+
+
 
 }
