2d9129c2bc52f40a79d3afe4b4f4b6d4ea3ad652
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-11 03:35:25 +0330

    10. Casting Types with the as Keyword

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 
 fn main() {
-let pi: f32 = 3.14159455654534545785545465411;
-println!("my pie is: {pi:.5}" );
-// or
-println!("my pie is: {:.5}", pi );
+let miles_away: i32 = 50;
+let miles_away_i8: i8 = miles_away as i8;
+// or different type 
+
+let miles_away_f64: f64 = 412452.45324523 ;
+let miles_away_i8: i16 = miles_away_f64 as i16 ;
+
+println!("my pie is:{miles_away_f64} {miles_away_i8}" );
 
 }
