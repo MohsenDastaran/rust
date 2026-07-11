3a8278342ce26b14336e67f0ecd0eccebae42401
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-11 23:34:04 +0330

    24. The Tuple Type

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 
 fn main() {
 let numbers: [i32; 12] = [20, 25, 35, 3, 3,23,3,23,23,32,323,232];
- 
-println!("{:?}" , numbers);
-//  or 
+
 println!("{numbers:#?}");
 
-
+println!("{}", dbg!(56465 / 6545) + 1)
 
 }
