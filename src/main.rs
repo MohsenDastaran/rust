81af9da575d921df51294314293a3949f8fb8eba
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-11 23:30:21 +0330

    21. The Display Trait for debugging with pretty print

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 
 fn main() {
-let numbers: [i32; 3] = [20, 25, 35];
+let numbers: [i32; 12] = [20, 25, 35, 3, 3,23,3,23,23,32,323,232];
  
 println!("{:?}" , numbers);
 //  or 
-println!("{numbers:?}");
+println!("{numbers:#?}");
 
 
 
