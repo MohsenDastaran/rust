4e9f06137cf7e5734965c990d8dfa9c1461c5fde
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-12 04:15:28 +0330

    4. Explicit Return Values

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 
 fn main() {
-learn_lang("Rust");
-learn_lang_via("Rust"     , "Course");
-// 1 parameter
-fn learn_lang(lang: &str) {
-    println!("Im learning {lang}")
-}
+    println!("{}", square(5));
+    
+    fn square(number: i32) -> i32 {
+        return number * number
+    }
 
 
-// 2 parameter 
-fn learn_lang_via(lang: &str, via: &str) {
-    println!("Im learning {lang} via {via}")
-}
+
+//     learn_lang_via("Rust"     , "Course");
+// // 2 parameter 
+
+//     fn learn_lang_via(lang: &str, via: &str)   -> str{ 
+//         return ("Im learning {lang} via {via}")
+//     }
+
 
 
 }
