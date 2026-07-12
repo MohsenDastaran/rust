adc2b6a1337f3b36bb710af1d56daa3446acffd8
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-12 04:05:26 +0330

    3. Parameters and Arguments p2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 
 fn main() {
 learn_lang("Rust");
-
-
+learn_lang_via("Rust"     , "Course");
+// 1 parameter
 fn learn_lang(lang: &str) {
     println!("Im learning {lang}")
+}
+
+
+// 2 parameter 
+fn learn_lang_via(lang: &str, via: &str) {
+    println!("Im learning {lang} via {via}")
 }
 
 
