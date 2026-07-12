5d4e2df4b6b99a86cf14db5431027a98da24af62
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-12 06:36:36 +0330

    6. The Unit as a Return Type

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
 
 fn main() {
-  let result = mystery();
-
-  println!("{:#?}", result);
-
-  
-    fn mystery() -> i32{
-        println!("dfg");
-        5
+    let result = 32;
+    {
+        
     }
 
-//     learn_lang_via("Rust"     , "Course");
-// // 2 parameter 
 
-//     fn learn_lang_via(lang: &str, via: &str)   -> str{ 
-//         return ("Im learning {lang} via {via}")
-//     }
+
+
 
 
 
