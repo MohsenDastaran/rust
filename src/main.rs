ca8bcf63b3b90abb0172c286565840070a1db684
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-12 06:39:37 +0330

    7. Blocks in Functions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 
 fn main() {
     let result = 32;
-    {
-        
-    }
+   let calc =  {
+        let inner_value = 5 + 4;
 
+        inner_value * result 
+    };
+    println!("{}", calc)
 
 
 
