7c4d7994ea9bd275b2df756f236f9695aeeaf7b1
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-14 01:40:13 +0330

    5. The Copy Trait

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 fn main() {
     let age: i8 = 29;
-    let is_handsome: bool = true;
-
-    println!("{age}");
-    println!("{is_handsome}");
-    // both exist here
+    let year: i8 = age;
 }
-
-// after scope, because of LIFO, first, is_handsome will be removed & after that, age.
