73d48b3c7e3e471313c4fd858663404cee8d0bb1
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-14 01:32:30 +0330

    4. Scope and Ownership

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-fn countdown(n: i32) {
-    if n > 0 {
-        println!("{}!", n);
-        countdown(n - 1);
-    } else {
-        println!("Liftoff!");
-    }
-}
-
 fn main() {
-    countdown(5);
+    let age: i8 = 29;
+
+    {
+        let is_handsome: bool = true;
+        // is_handsome is in memory here
+    }
+    // println!("{is_handsome}")
+    // is_handsome is not in memory because its out of scope
 }
