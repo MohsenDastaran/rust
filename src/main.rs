8ace66a7bdb4b0fe32b304f2a32ec6e2c5bd3669
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-14 05:08:54 +0330

    6. The String Type 2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
-    // store in executable
-    let food: &str = "pasta";
+    // store in binary executable
+    let mut food: &str = "pasta";
+    println!("{food}");
+
+    food = "xfgcxfg";
+    println!("{food}");
 
     // store in heap, its gonna change
     let text: String = String::new();
     let candy = String::from("Kitkat");
 
-    println!("{candy}")
+    println!("{candy}");
 }
