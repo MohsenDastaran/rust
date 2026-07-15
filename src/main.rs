82f72cd783324c2e95772553e117d5a2f65a98b8
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-15 07:15:55 +0330

    13. String, &String, str, and &str

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 fn main() {
-    // stack data
-    let my_stack_value: i32 = 2;
-    let my_intger_refrence: &i32 = &my_stack_value; // borrow with &
-    println!("{my_intger_refrence}");
+    /*
+    String - A dynamic piece of text stored on the heap
+    at runtime
 
-    // ////////////////////////////////////////////// //
+    &String ("ref String") - A reference to a heap String
 
-    // heap data
+    str - A hardcoded, read-only piece of text encoded in
+    the binary
+
+    &str ("ref str") - A reference to the text in the memory
+    that has loaded the binary file
+    */
+
+    // String in heap data
     let my_heap_value: String = String::from("value");
+    // String reference in heap data
     let my_heap_refrence: &String = &my_heap_value;
 
-    println!("{my_heap_refrence}");
+    // A reference to text stored in the program's read-only memory.
+    let my_str: &str = "My Text";
+
+    // Why is this &str and not str?
+    // Because `str` is an unsized type and cannot be stored directly in a variable.
+    // `my_str` is a reference to the string literal "My Text", which is stored
+    // in the program's binary (read-only memory).
+    println!("{my_heap_refrence} {my_str}");
 }
