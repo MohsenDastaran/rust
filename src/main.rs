c9b975c4f551bff7fcaa6a998dd74c6a17804426
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-15 07:01:48 +0330

    11. References and Borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let person: String = String::from("Mohsen");
+    // stack data
+    let my_stack_value: i32 = 2;
+    let my_intger_refrence: &i32 = &my_stack_value; // borrow with &
+    println!("{my_intger_refrence}");
 
-    // this will not move ownership, it clones the value, different heap memory
-    let mo: String = person.clone();
+    // ////////////////////////////////////////////// //
 
-    println!("{}", person);
-    println!("{}", mo);
+    // heap data
+    let my_heap_value: String = String::from("value");
+    let my_heap_refrence: &String = &my_heap_value;
+
+    println!("{my_heap_refrence}");
 }
