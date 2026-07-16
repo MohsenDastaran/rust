1f74f2962b88b72e6f1b3ca80b874593d4339a9f
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-16 09:08:54 +0330

    17. Return Values II

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    let cake: String = bake_cake();
-    println!("I baked a {}!", cake);
+    let current_meal: String = String::new();
+    let fl: String = add_flour(current_meal);
+    let su: String = add_sugar(fl);
+
+    println!("{}! ", su);
 }
 
-fn bake_cake() -> String {
-    String::from("Chocolate cake") //  last expression is returned
-    // if we dont return the value, the value will be dropped
+fn add_flour(mut meal: String) -> String {
+    meal.push_str(" with flour");
+    meal
 }
+
+fn add_sugar(mut meal: String) -> String {
+    meal.push_str(" with sugar");
+    meal
+}
+
+//  HOW TO AVOID RETURNING THE VALUE OF A VARIABLE IN RUST:
+
+// Next Section is about that: Refrences
