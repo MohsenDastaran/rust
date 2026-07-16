7a346b11f0d012855cce6ccdda751a0997a68a11
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-16 08:52:05 +0330

    16. Mutable Parameters

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,8 @@
-// with error[E0382]: borrow of moved value: `apples`
-
-// fn main() {
-//     let apples: String = String::from("apples");
-
-//     print_hello(apples);
-//     println!("Hello 2, {}!", apples);
-// }
-// fn print_hello(value: String) {
-//     println!("Hello, {}!", value);
-// }
-
-// without error:
 fn main() {
-    let apples: String = String::from("apples");
-
-    let result: String = print_hello(apples);
-    println!("Hello 2, {}!", result);
+    let burger: String = String::from("burger");
+    add_fries(burger);
 }
-fn print_hello(value: String) -> String {
-    println!("Hello, {}!", value);
-    return value;
+fn add_fries(mut meal: String) {
+    meal.push_str(" with fries");
+    println!("Meal: {}", meal);
 }
