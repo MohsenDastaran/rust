682989f27dc13ff7a40d2013d2a743ab2785906a
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-17 17:08:25 +0330

    6. Create Structs in a Function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
+struct Cofee {
+    name: String,
+    price: f64,
+    is_hot: bool,
+}
+
 fn main() {
-    #[derive(Debug)]
-    struct Cofee {
-        name: String,
-        price: f64,
-        is_hot: bool,
+    let cofee: Cofee = make_coffee("name".into(), 3.5, true);
+    println!("{}", cofee.name);
+}
+fn make_coffee(name: String, price: f64, is_hot: bool) -> Cofee {
+    Cofee {
+        name,
+        price,
+        is_hot,
     }
-
-    let mut mocha: Cofee = Cofee {
-        name: String::from("Mocha"),
-        price: 3.5,
-        is_hot: true,
-    };
-
-    mocha.name = String::from("Cappuccino");
-    println!("{:#?}", mocha);
 }
