02c73fd41c18223676022bc827fc368464e3cce2
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-17 23:52:16 +0330

    8. Struct Update Syntax

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,18 @@ struct Cofee {
 fn main() {
     let cofee: Cofee = make_coffee("name".into(), 3.5, true);
     println!("{}", cofee.name);
+
+    //  new cofee with ..
+    let caramel_macchiato = Cofee {
+        name: String::from("Caramel Macchiato"),
+        ..cofee // it should be on last line of the struct
+    };
+
+    println!("{}", caramel_macchiato.name);
+
+    // update the name of the coffee
 }
+
 fn make_coffee(name: String, price: f64, is_hot: bool) -> Cofee {
     Cofee {
         name,
