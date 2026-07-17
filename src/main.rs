695bd75f2c21490816e78d1989dc90bbb6a97bc1
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-17 06:41:35 +0330

    7. Ownership with Arrays and Tuples

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 fn main() {
-    let city = create_city();
-    println!("City: {}", city);
-}
+    let registrations: [bool; 3] = [true, false, true];
+    let first: bool = registrations[0];
 
-// dangling reference example, city will be dropped at the end of the function and the reference will be invalid
-fn create_city() -> String {
-    let city = String::from("Sari");
-    city
+    println!(
+        "First registration status: {} & {:#?}",
+        first, registrations
+    );
+
+    // example for Array of heap allocated strings
+    let langs: [String; 3] = [
+        String::from("Rust"),
+        String::from("Python"),
+        String::from("JavaScript"),
+    ];
+    // error: String does not implement the `Copy` trait, so we cannot directly assign it to a variable like this. We need to clone it or use a reference.
+    // let first_lang: String = langs[0];
+
+    // Correct way to get the first language because we are borrowing it, not taking ownership
+    let first_lang: &String = &langs[0];
+
+    // or clone it if you need ownership
+    // let first_lang: String = langs[0].clone();
+
+    println!("First Language status: {} & {:#?}", first_lang, langs);
 }
