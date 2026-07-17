fee20d1fb752e11eb401510919a7d10d78c49eec
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-17 16:52:44 +0330

    3. Create a Struct Instance

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,11 @@ fn main() {
         price: f64,
         is_hot: bool,
     }
+
+    let mocha: Cofee = Cofee {
+        name: String::from("Mocha"),
+        price: 3.5,
+        is_hot: true,
+    };
+    // println!("{:#?}", mocha);
 }
