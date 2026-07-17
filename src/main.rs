d38b1922f268fc7aaf72225ad268fd1cafe925c8
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-17 09:44:53 +0330

    3. Create a String Slice from a String

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
 fn main() {
-    let registrations: [bool; 3] = [true, false, true];
-    let first: bool = registrations[0];
+    let actor: String = String::from("Keanu Reeves");
 
-    println!(
-        "First registration status: {} & {:#?}",
-        first, registrations
-    );
+    let string_ref: &str = &actor[0..8];
 
-    // example for Array of heap allocated strings
-    let langs: [String; 3] = [
-        String::from("Rust"),
-        String::from("Python"),
-        String::from("JavaScript"),
-    ];
-    // error: String does not implement the `Copy` trait, so we cannot directly assign it to a variable like this. We need to clone it or use a reference.
-    // let first_lang: String = langs[0];
+    println!("Actor: {}", string_ref);
 
-    // Correct way to get the first language because we are borrowing it, not taking ownership
-    let first_lang: &String = &langs[0];
-
-    // or clone it if you need ownership
-    // let first_lang: String = langs[0].clone();
-
-    println!("First Language status: {} & {:#?}", first_lang, langs);
+    let last_name = &actor[6..];
+    println!("Last Name: {}", last_name);
 }
