6e62d7ba48a73dc089cfea9b7e9ccc54ebfca158
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-17 05:01:59 +0330

    4. Mutable Reference Restrictions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let  car: String = String::from("Red");
-    let ref1: &String = &car; // reference to car
+    let mut car: String = String::from("Red");
+    let ref1: &String = &mut car; // reference to car
     let ref2: &String = &car; // another reference to car
-
 
     println!("ref1: {}, ref2: {}", ref1, ref2);
 }
-
