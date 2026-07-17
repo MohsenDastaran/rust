7afb70efc05518acdba8af8c6825da85ead3a69d
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-17 16:49:17 +0330

    2. Define a Struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    let values: [i32; 4] = [7452, 1234, 5678, 9101];
-    let my_slice: &[i32] = &values[..2];
-    println!("Last Name: {:?} {:?}", my_slice, values);
+    struct Cofee {
+        name: String,
+        price: f64,
+        is_hot: bool,
+    }
 }
