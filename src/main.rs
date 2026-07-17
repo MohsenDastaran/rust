2757f5300043943db316f1907d1d7b5a093be31f
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-17 14:59:49 +0330

    4. String Slices and String Literals

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    let actor: String = String::from("Keanu Reeves");
-
-    let string_ref: &str = &actor[0..8];
-
-    println!("Actor: {}", string_ref);
-
-    let last_name = &actor[6..];
-    println!("Last Name: {}", last_name);
+    let first_name = {
+        let actor: &str = "Keanu Reeves";
+        &actor[0..8]
+    };
+    println!("Last Name: {}", first_name);
 }
