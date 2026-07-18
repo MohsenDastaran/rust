a7426c1c1d425329bad2b8fbc897c3e3683bcd61
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-18 16:22:44 +0330

    2. Intro to Enums

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,15 @@
 #[derive(Debug)]
-struct Computer {
-    cpu: String,
-    memory: i32,
-    hard_drive: i32,
+enum  CardSuit {
+    Heart, Diamond, Spades, Clubs
 }
-
-impl Computer {
-    fn new(cpu: String, memory: i32, hard_drive: i32) -> Self {
-        Self {
-            cpu,
-            memory,
-            hard_drive,
-        }
-    }
-    fn upgrade_cpu(&mut self, new_cpu: String) -> &mut Self {
-        self.cpu = new_cpu;
-        self
-    }
-    fn upgrade_memory(&mut self, new: i32) -> &mut Self{
-        self.memory = new;
-        self
-    }
-    fn upgrade_hard_drive(&mut self, new_space: i32)-> &mut Self {
-        self.hard_drive = new_space;
-        self
-    }
+struct Card {
+    rank: String,
+    suit: CardSuit
 }
 fn main() {
-    let mut my_computer = Computer::new("Intel".to_string(), 16, 512);
+    let mut first_card = CardSuit::Diamond;
 
-
-    println!("Hello, {:?}", my_computer);
-
-    my_computer.upgrade_cpu("AMD".to_string()).upgrade_hard_drive(120).upgrade_memory(16);
-
-    println!("New, {:?}", my_computer);
+    // first_card = "sdc"            //expected CardSuit, found &'static str
+    println!("{:#?}", first_card);
 
 }
