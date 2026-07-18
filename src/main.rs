38133fc2bd20c842088ea0b7f519f45e5538463c
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-18 13:13:42 +0330

    13. self Parameter as Mutable References to Struct Instance

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ struct Song {
     duration_secs: u64,
 }
 impl Song {
-    fn display_song_info(self) {
+    fn display_song_info(&self) {
         // we have options for self:
         // self: takes ownership of the instance
         //  passing immutably: &self (self parameter takes ownership of the instance)
@@ -16,16 +16,17 @@ impl Song {
         println!("Release Year: {}", self.release_year);
         println!("Duration Seconds: {}", self.duration_secs);
     }
-    fn double_duration(mut self) {
+    fn double_duration(&mut self) {
         self.duration_secs *= 2;
         println!("Doubled Duration Seconds: {:?}", self.duration_secs);
     }
 }
 fn main() {
-    let song = Song {
+    let mut song = Song {
         title: "Creep".into(),
         release_year: 2020,
         duration_secs: 200,
     };
     song.double_duration();
+    song.display_song_info();
 }
