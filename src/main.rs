a3a6cc53a1488b2d19fa677eb5ba80f782268b5e
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 03:20:13 +0330

    7. Nesting Enums in Enums

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-
+#[derive(Debug)]
+enum Beans {
+    Black,
+    Pinto,
+}
 
 
 #[derive(Debug)]
-enum  PaymentMethodType {
-   CreditType(String),
-   Paypal{username: String, password: String} // struct variant
+enum Meat {
+    Chicken,
+    Beaf,
+}
 
+#[derive(Debug)]
+
+enum ResaurantItem {
+    Burrito(Meat),
+    Bowl {meat: Meat, beans: Beans},
+    Vegan,
 }
 
 fn main() {
-let visa: PaymentMethodType = PaymentMethodType::CreditType(String::from("4544-6545-654-654"));
-    println!("{:?}", visa);
-    let pay: PaymentMethodType = PaymentMethodType::Paypal { username: String::from("654"), password: String::from("4544-654dfsdvsdvsdvsdv5-654-654") };
-    println!("{:?}",pay);
+    let lunch = ResaurantItem::Burrito(Meat::Chicken);
+    let dinner = ResaurantItem::Burrito(Meat::Beaf);
+
+    let brunch = ResaurantItem::Bowl {meat: Meat::Beaf, beans: Beans::Black };
+
+    println!("{:?}, {:?}, {brunch:?}", lunch, dinner)
 }
