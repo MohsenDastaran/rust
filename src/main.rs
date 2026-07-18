85c2f4ad14f86fa506312eb27f04c1cfa244ca19
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 02:55:19 +0330

    Add second parameter to enum

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #[derive(Debug)]
 enum  PaymentMethodType {
    CreditType(String),
-   Paypal(String)
+   Paypal(String, String)
 
 }
 
 fn main() {
-let visa = PaymentMethodType::CreditType(String::from("4544-6545-654-654"));
+let visa: PaymentMethodType = PaymentMethodType::CreditType(String::from("4544-6545-654-654"));
     println!("{:?}", visa);
-
+    let pay: PaymentMethodType = PaymentMethodType::Paypal(String::from("654"), String::from("4544-654dfsdvsdvsdvsdv5-654-654"));
+    println!("{:?}",pay);
 }
