c0bf3b066f0e8faad281bb10e04a7cfb144da9fe
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 03:02:45 +0330

    6. Struct Variants

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+
+
+
 #[derive(Debug)]
 enum  PaymentMethodType {
    CreditType(String),
-   Paypal(String, String)
+   Paypal{username: String, password: String} // struct variant
 
 }
 
 fn main() {
 let visa: PaymentMethodType = PaymentMethodType::CreditType(String::from("4544-6545-654-654"));
     println!("{:?}", visa);
-    let pay: PaymentMethodType = PaymentMethodType::Paypal(String::from("654"), String::from("4544-654dfsdvsdvsdvsdv5-654-654"));
+    let pay: PaymentMethodType = PaymentMethodType::Paypal { username: String::from("654"), password: String::from("4544-654dfsdvsdvsdvsdv5-654-654") };
     println!("{:?}",pay);
 }
