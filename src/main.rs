8b6e2219ef9a4b7ad9bb78254579399c9a3a7024
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 07:31:05 +0330

    15. The let else Construct

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,11 @@ enum Milk {
 
 fn main() {
     let my_beverage = Milk::Whole;
-    if let Milk::Whole = my_beverage {
-        println!("fdgdfgdfg")
-    }
 
-    let my_beverage2 = Milk::Lowfat(12);
-
-    if let Milk::Lowfat(percent) = my_beverage2 {
-        println!("fdgdfgdfg {percent}")
-    }
-
-    let my_beverage3 = Milk::NonDiary {
-        kind: "Hahaaaaaa".into(),
+    let Milk::Lowfat(percent) = my_beverage else {
+        println!("There is no percent here");
+        return;
     };
 
-    if let Milk::NonDiary { kind } = my_beverage3 {
-        println!("Kind: {kind}")
-    }
+    println!("{}", percent)
 }
