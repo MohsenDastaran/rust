77776b0a06ee44ea4fbb9966af8dc1bc60f5bfa1
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 12:06:27 +0330

    4. The unwrap and expect Methods

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    let instruments = [
+    let instruments: [String; 3] = [
         String::from("Guitar"),
         String::from("Piano"),
         String::from("Tar"),
     ];
 
-    let base: Option<&String> = instruments.get(10);
-    println!("{base:?}")
+    let base: Option<&String> = instruments.get(20);
+    println!("{base:?}");
+    // println!("{}", base.unwrap()); // to get the raw value
+    println!("{}", base.expect("Error Happened Mohsen")) // to get the raw value with Error handling
 }
