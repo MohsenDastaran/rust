e98143ca3137bc8a60e2dbc9f0d79f25c108b69e
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 11:53:32 +0330

    2. The Option Enum

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
 fn main() {
-    println!("{}", add_5(5));
-    identity::<i32>(5);
-    identity::<&str>("sdfsdvc");
-    identity(true);
+    let a = Option::Some(23); // some represent that we have the 
+    let b = Option::Some("Hello"); // some represent that we have 
 
-    identity2("sdcsdc", 36.56)
-}
-// normal parameter type
-fn add_5(value: i32) -> i32 {
-    value + 5
-}
-
-//  but here, i want a function to have dynamic value
-fn identity<T: std::fmt::Debug>(value: T) {
-    println!("{:?}", value)
-}
-fn identity2<T: std::fmt::Debug, U: std::fmt::Debug>(value: T, another: U) {
-    println!("{:?}, {:?}", value, another)
+    let c: Option<String> = Option::None;
 }
