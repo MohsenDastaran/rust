5f927f2249f724cf06acb5d27bd494dd5bfe3fce
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 15:16:06 +0330

    2. Create a Vector

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let ok: Result<i32, String> = Result::Ok(5);
-    let disaster: Result<i32, &str> = Result::Err("Error");
-    println!("{ok:?}");
-    println!("{disaster:?}");
+    // let movies = ["Se7en", "Titanic"]; // array has fixed length
+    // movies.push("Usual suspect")  // doesnt work
+
+    // let vec_movies: Vec<String> = Vec::<String>::new();  //empty
+    let vec_movies: Vec<String> = vec!["Se7en".into(), "Titanic".into()];
+    println!("{:?}", vec_movies)
 }
