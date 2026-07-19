b356dfd1cd2f714e68a42bfc1f094ce9cfbea3f5
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 11:25:36 +0330

    4. Multiple Generics

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ fn main() {
     identity::<i32>(5);
     identity::<&str>("sdfsdvc");
     identity(true);
+
+    identity2("sdcsdc", 36.56)
 }
 // normal parameter type
 fn add_5(value: i32) -> i32 {
@@ -12,4 +14,7 @@ fn add_5(value: i32) -> i32 {
 //  but here, i want a function to have dynamic value
 fn identity<T: std::fmt::Debug>(value: T) {
     println!("{:?}", value)
+}
+fn identity2<T: std::fmt::Debug, U: std::fmt::Debug>(value: T, another: U) {
+    println!("{:?}, {:?}", value, another)
 }
