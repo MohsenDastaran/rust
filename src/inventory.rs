2dda6a49b26245fa8c2208c2c1f727f6920453a7
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-19 22:24:38 +0330

    6. Module as File

---
+++ src/inventory.rs
@@ -0,0 +1,17 @@
+pub const FLOOR_SPACE: i32 = 10000;
+pub const MANAGER: &str = "Mohsen Dastaran";
+
+#[derive(Debug)]
+enum ProductCategory {
+    Ladder,
+    Hammer,
+}
+struct Item {
+    name: String,
+    category: ProductCategory,
+    quantity: u32,
+}
+
+pub fn talk_to_manager() {
+    println!("Hey {MANAGER}, Hahahaha")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
-mod inventory {
-    pub const FLOOR_SPACE: i32 = 10000;
-    pub const MANAGER: &str = "Mohsen Dastaran";
-
-    #[derive(Debug)]
-    enum ProductCategory {
-        Ladder,
-        Hammer,
-    }
-    struct Item {
-        name: String,
-        category: ProductCategory,
-        quantity: u32,
-    }
-
-    pub fn talk_to_manager() {
-        println!("Hey {MANAGER}, Hahahaha")
-    }
-}
+mod inventory;
 
 fn main() {
     // println!("Hey {MANAGER}") // cannot access manager
