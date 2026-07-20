2feff8625321290e03e6a33c06859a66e61428ce
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-20 23:28:36 +0330

    16. Create Aliases with the as Keyword

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod inventory;
 
-use inventory::products; // for removing the inventory:: prefix for manager
+use inventory::products as P; // for removing the inventory:: prefix for manager
 fn main() {
     // println!("Hey {MANAGER}") // cannot access manager
     // println!("Hey {}", inventory::MANAGER);
     // println!("Hey {}", inventory::products::FLOOR_SPACE);
 
     // inventory::talk_to_manager();
-    let a = products::Item::new("name".into(), products::ProductCategory::Hammer, 12);
+    let a = P::Item::new("name".into(), P::ProductCategory::Hammer, 12);
 }
