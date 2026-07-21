a8e8203d9183c8f94a04d211c7d85f6f6e1b8af7
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-21 08:48:08 +0330

    18. External Crates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod inventory;
 use fake::{Fake, Faker};
+
+use crate::inventory::products::ProductCategory;
+
 fn main() {
     let fake_items: inventory::Item = Faker.fake();
     println!("{:?}", fake_items);
+
+    let random_category: ProductCategory = Faker.fake();
+    println!("{:?}", random_category);
 }
