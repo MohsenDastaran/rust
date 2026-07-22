6643d2bb0348784013902b15192d3a4e50bbc80e
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-22 06:31:38 +0330

    3. The process Module and the exit Function

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use std::process;
+
+fn main() {
+    let a = 5;
+    process::exit(0); // 0 means no error
+    process::exit(1); // 01 or more means error: unreachable statement
+
+    println!("sdv") // unreachable
+}
