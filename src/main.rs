f4fe33f217a429e2a650b57091a8bad081461f3d
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-22 19:06:25 +0330

    5. Implementing Trait for Struct II

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::format};
+
+fn main() {
+    let mut hotel = Hotel::new("California");
+    println!("{}", hotel.get_description());
+    hotel.book("Asghar", 4);
+    println!("{:#?}", hotel);
+
+    let mut air_bnb = AirBnB::new("Mohsen");
+    println!("{}", air_bnb.get_description());
+    air_bnb.book("Ali", 8);
+    println!("{:?}", air_bnb);
+}
 
 trait Accomomdation {
     fn get_description(&self) -> String;
@@ -20,11 +32,32 @@ impl Hotel {
 
 impl Accomomdation for Hotel {
     fn get_description(&self) -> String {
-        format!("{} is a luxury", self.name)
+        format!("Welcome to the Hotel {}", self.name)
     }
     fn book(&mut self, name: &str, nights: u32) {
         self.reservations.insert(name.to_string(), nights);
     }
 }
+#[derive(Debug)]
 
-fn main() {}
+struct AirBnB {
+    host: String,
+    guests: Vec<(String, u32)>,
+}
+
+impl AirBnB {
+    fn new(host: &str) -> Self {
+        Self {
+            host: host.to_string(),
+            guests: vec![],
+        }
+    }
+}
+impl Accomomdation for AirBnB {
+    fn get_description(&self) -> String {
+        format!("Please Enjoy {}'s Room", self.host)
+    }
+    fn book(&mut self, name: &str, nights: u32) {
+        self.guests.push((name.to_string(), nights))
+    }
+}
