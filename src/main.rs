ff335e311c50c7bfebe94da79f0f3ab992845fe9
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-22 10:04:35 +0330

    10. The Operator (The Try Operator)

--- src/main.rs
+++ src/main.rs
@@ -18,25 +18,13 @@ fn read_file() -> Result<String, io::Error> {
 
     let mut input = String::new();
 
-    // another better way of error handling:
-    let user_req_file = stdin().read_line(&mut input);
+    stdin().read_line(&mut input)?;
 
-    // if error hapened
-    if let Err(error) = user_req_file {
-        return Err(error);
-    }
-
-    let mut story: File = match File::open(input.trim()) {
-        Ok(file) => file,
-        Err(error) => return Err(error),
-    };
-    // println!("{:?}", story);
+    let mut story = File::open(input.trim())?;
 
     let mut file_contents = String::new();
-    let read_operation = story.read_to_string(&mut file_contents);
-    if let Err(error) = read_operation {
-        return Err(error);
-    }
-    // println!("{}", file_contents);
+
+    story.read_to_string(&mut file_contents)?;
+
     return Ok(file_contents);
 }
