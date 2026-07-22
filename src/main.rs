f8f9b13c2f86076ffeb98ee60b037b963de56ba6
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-22 10:19:25 +0330

    12. Using with Option

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-use std::fs;
-use std::process;
-
-use std::io::{self, stdin};
 fn main() {
-    let content: Result<String, io::Error> = read_file();
-    match content {
-        Ok(i) => println!("{i}"),
-        Err(error) => {
-            eprintln!("Error Happened, File is missing: {error:?}");
-            process::exit(1)
-        }
-    }
+    let mut animals = vec!["zebra", "monkey", "leopard"];
+    println!("{:?}", length_of_last_element(&mut animals));
+    println!("{:?}", length_of_last_element(&mut animals));
+    println!("{:?}", length_of_last_element(&mut animals));
+    println!("{:?}", length_of_last_element(&mut animals));
+    println!("{:?}", length_of_last_element(&mut animals));
+    println!("{:?}", length_of_last_element(&mut animals))
 }
 
-fn read_file() -> Result<String, io::Error> {
-    println!("Please enter the file path");
-
-    let mut input = String::new();
-
-    stdin().read_line(&mut input)?;
-    fs::read_to_string(input.trim())
+fn length_of_last_element(input: &mut Vec<&str>) -> Option<usize> {
+    let last = input.pop()?;
+    Some(last.len())
 }
