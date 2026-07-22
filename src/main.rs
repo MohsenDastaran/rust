c75276aca59f8f4e29437c43b7f971228fdfa03e
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-22 07:03:37 +0330

    6. Asking the User for Input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use std::fs::File;
 use std::process;
+
+use std::io::stdin;
 fn main() {
-    // std::io::Error is for when the file doesnt exist
-    let story: File = match File::open("./story.txt") {
+    println!("Please enter the file path");
+
+    let mut input = String::new();
+
+    // match stdin().read_line(&mut input) {
+    //     Ok(size) => println!("The Size!!!"),
+    //     Err(error) => {
+    //         println!("Something Went Wrong!");
+    //         process::exit(2)
+    //     }
+    // }
+
+    // another better way of error handling:
+    let user_req_file = stdin().read_line(&mut input);
+
+    // if error hapened
+    if let Err(_error) = user_req_file {
+        println!("Something Went Wrong!");
+        process::exit(2)
+    }
+
+    let story: File = match File::open(input.trim()) {
         Ok(file) => file,
         Err(error) => {
-            eprintln!("Error Happened, File is missing");
+            eprintln!("Error Happened, File is missing: {error:?}. user output: {input}");
             process::exit(1)
         }
     };
