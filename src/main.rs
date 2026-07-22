1de440c9438260a085f57783c26981ff94b1fcc4
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-22 04:33:09 +0330

    5. Access a Value by Key

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,17 @@ fn main() {
     menu.insert("Gheyme".into(), 19.00);
     menu.insert("Kabab".into(), 24.99);
 
-    println!("Testing Main {:?}", menu);
-
     let mut country_capitals = HashMap::<&str, &str>::new(); // with turbo-fish operator => ::<&str, &str>::
 
     country_capitals.insert("Iran", "Tehran");
     country_capitals.insert("Germany", "Berlin");
-    country_capitals.remove("Iran");
 
-    println!("Testing Main {:?}", country_capitals);
+    println!("{}", menu["Ghorme"]); // the problem is if value doesnt exist, this will cause panic in runtime
+
+    let value = country_capitals
+        .get("Germafny") // see the type is Option, because it can be Some or None ,
+        .copied() //use .copied() to remove 1 reference(&)
+        .unwrap_or("Unknown Country"); // add default  
+
+    println!("{:?}", value);
 }
