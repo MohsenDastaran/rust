87568ea52033d5435a9a83e9f56edaafd16d5a43
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-22 09:57:05 +0330

    8. Propagating Errors

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,42 @@
 use std::fs::File;
 use std::process;
 
-use std::io::{Read, stdin};
+use std::io::{self, Read, stdin};
 fn main() {
+    let content: Result<String, io::Error> = read_file();
+    match content {
+        Ok(i) => println!("{i}"),
+        Err(error) => {
+            eprintln!("Error Happened, File is missing: {error:?}");
+            process::exit(1)
+        }
+    }
+}
+
+fn read_file() -> Result<String, io::Error> {
     println!("Please enter the file path");
 
     let mut input = String::new();
-
-    // match stdin().read_line(&mut input) {
-    //     Ok(size) => println!("The Size!!!"),
-    //     Err(error) => {
-    //         println!("Something Went Wrong!");
-    //         process::exit(2)
-    //     }
-    // }
 
     // another better way of error handling:
     let user_req_file = stdin().read_line(&mut input);
 
     // if error hapened
-    if let Err(_error) = user_req_file {
-        eprintln!("Something Went Wrong!");
-        process::exit(2)
+    if let Err(error) = user_req_file {
+        return Err(error);
     }
 
     let mut story: File = match File::open(input.trim()) {
         Ok(file) => file,
-        Err(error) => {
-            eprintln!("Error Happened, File is missing: {error:?}. user output: {input}");
-            process::exit(1)
-        }
+        Err(error) => return Err(error),
     };
-    println!("{:?}", story);
+    // println!("{:?}", story);
 
     let mut file_contents = String::new();
     let read_operation = story.read_to_string(&mut file_contents);
-    if let Err(_error) = read_operation {
-        eprintln!("Something Went Wrong!");
-        process::exit(2)
+    if let Err(error) = read_operation {
+        return Err(error);
     }
-    println!("{}", file_contents);
+    // println!("{}", file_contents);
+    return Ok(file_contents);
 }
