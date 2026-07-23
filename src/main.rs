e7a3a2504f05b90d4bed357a1e9fb0a2a98f8e56
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-23 17:45:49 +0330

    13. The for_each Method

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-use std::collections::HashMap;
+use std::{char, collections::HashMap};
 
-fn count_words(text: &str) -> HashMap<char, u16> {
+fn count_charachters(text: &str) -> HashMap<char, u16> {
     let words = text.split_whitespace();
     let mut counts = HashMap::new();
-    // let mut letters = HashMap::new();
-    for word in words {
-        for character in word.chars() {
-            let count = counts.entry(character).or_insert(0);
+    // for word in words {
+    //     for character in word.chars() {
+    //         let count = counts.entry(character).or_insert(0);
+    //         *count += 1;
+    //     }
+    // }
+    // counts
+
+    // using for_each
+    words.for_each(|word| {
+        word.chars().for_each(|character| {
+            let count: &mut u16 = counts.entry(character).or_insert(0);
             *count += 1;
-        }
-        // let count = counts.entry(word).or_insert(0);
-        // *count += 1
-    }
+        });
+    });
     counts
 }
 
 fn main() {
     println!(
         "{:?}",
-        count_words("sdfsdcvs  ds ds sd sd d sd sdf sd sd sd sd sd sd sd sd ")
+        count_charachters("sdfsdcvs  ds ds sd sd d sd sdf sd sd sd sd sd sd sd sd ")
     )
 }
