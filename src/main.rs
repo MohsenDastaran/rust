e1a93879a47481eaa66f4dc0f9a1519f63f320d4
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-23 19:28:23 +0330

    14. The map Method II

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, mem::replace};
 
 fn main() {
-    let numbers: Vec<i32> = vec![
-        1, 5, 4, 54, 54, 54, 654, 65, 132, 132, 132, 1321, 3651, 589, 7984, 854, 354,
+    let names = [
+        String::from("Mohsen"),
+        String::from("Ali"),
+        String::from("Pouya"),
     ];
 
-    // using Vec<i32>
-
-    // let squares = numbers
-    //     .iter()
-    //     .map(|num: &i32| num.pow(2))
-    //     .collect::<Vec<i32>>();
-
-    // using HashSet<i32>
-    let squares = numbers
+    let name_lengths = names
         .iter()
-        .map(|num: &i32| num.pow(2))
-        .collect::<HashSet<i32>>();
-    println!("{squares:?}");
+        .map(|name| name.to_lowercase())
+        .map(|name| name.replace("i", "@@"))
+        .map(|name| name.len())
+        .collect::<Vec<usize>>();
+
+    println!("{:?}", name_lengths)
 }
