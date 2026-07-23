2da6e82ad439e5487c542016ccab8f65e29f669b
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-23 19:18:49 +0330

    16. The map Method II

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::collections::HashSet;
+
 fn main() {
     let numbers: Vec<i32> = vec![
         1, 5, 4, 54, 54, 54, 654, 65, 132, 132, 132, 1321, 3651, 589, 7984, 854, 354,
     ];
 
-    let squares = numbers.into_iter().map(|num: i32| num.pow(2));
-    println!("{squares:?}"); // same as numbers
+    // using Vec<i32>
 
-    for num in squares {
-        println!("{num:?}"); // now, we can see the real values becaue of the laziness 
-    }
+    // let squares = numbers
+    //     .iter()
+    //     .map(|num: &i32| num.pow(2))
+    //     .collect::<Vec<i32>>();
+
+    // using HashSet<i32>
+    let squares = numbers
+        .iter()
+        .map(|num: &i32| num.pow(2))
+        .collect::<HashSet<i32>>();
+    println!("{squares:?}");
 }
