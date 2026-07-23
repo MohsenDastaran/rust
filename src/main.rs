2b37aed0e902f7cc31843b649211ee1da340e38e
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-23 13:06:12 +0330

    5. Exhausting the Iterator

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use std::collections::{HashMap, hash_map};
 
 fn main() {
-    let my_vetor = vec![5, 5, 5, 214, 5];
-    let my_iterable = my_vetor.into_iter(); // make vector iterable
+    let my_vetor = vec![5, 12, 500, 214, 5];
+    let mut my_iterable = my_vetor.into_iter(); // make vector iterable
+
+    println!("{:?}", my_iterable.next());
+    println!("{:?}", my_iterable.next());
+    println!("{:?}", my_iterable);
 
     // bool
-    let my_vetor_bool: Vec<bool> = vec![true, false, true];
-    let my_iterable_bool = my_vetor_bool.into_iter(); // make vector iterable
+    // let my_vetor_bool: Vec<bool> = vec![true, false, true];
+    // let my_iterable_bool = my_vetor_bool.into_iter(); // make vector iterable
 
-    //Hashmap
-    let mut my_hashmap: HashMap<&str, i32> = HashMap::new();
-    my_hashmap.insert("CBS", 2);
-    my_hashmap.insert("Intl", 3);
-    my_hashmap.insert("BBC", 4);
+    // //Hashmap
+    // let mut my_hashmap: HashMap<&str, i32> = HashMap::new();
+    // my_hashmap.insert("CBS", 2);
+    // my_hashmap.insert("Intl", 3);
+    // my_hashmap.insert("BBC", 4);
 
-    println!("{:?}", my_hashmap);
-    let my_iterable_hashmap: hash_map::IntoIter<&str, i32> = my_hashmap.into_iter(); // make vector iterable
+    // println!("{:?}", my_hashmap);
+    // let my_iterable_hashmap: hash_map::IntoIter<&str, i32> = my_hashmap.into_iter(); // make vector iterable
 
-    println!("{:?}", my_iterable_hashmap);
+    // println!("{:?}", my_iterable_hashmap);
 }
