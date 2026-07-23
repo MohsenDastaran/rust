5ee56b967e1e0a64acde5d8e4c7676dce4777ba2
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-23 17:40:13 +0330

    12. Solving a Problem with Iteration

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+use std::collections::HashMap;
+
+fn count_words(text: &str) -> HashMap<char, u16> {
+    let words = text.split_whitespace();
+    let mut counts = HashMap::new();
+    // let mut letters = HashMap::new();
+    for word in words {
+        for character in word.chars() {
+            let count = counts.entry(character).or_insert(0);
+            *count += 1;
+        }
+        // let count = counts.entry(word).or_insert(0);
+        // *count += 1
+    }
+    counts
+}
+
 fn main() {
-    let my_str = "value🍵";
-
-    // loop over each byte
-    for byte in my_str.bytes() {
-        println!("{byte}")
-    }
-    // loop over each char
-
-    for ch in my_str.chars() {
-        println!("{ch}")
-    }
+    println!(
+        "{:?}",
+        count_words("sdfsdcvs  ds ds sd sd d sd sdf sd sd sd sd sd sd sd sd ")
+    )
 }
