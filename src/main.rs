cea76c9c3d5f3f3ec29d35b1b3d68bda5a8ad475
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-23 13:38:17 +0330

    10. HashMap Iteration

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,15 @@
-use std::collections::{HashMap, hash_map};
+use std::collections::HashMap;
 
 fn main() {
-    let my_vetor = vec![5, 12, 500, 214, 5];
-    let mut my_iterable = my_vetor.into_iter(); // make vector iterable
+    let mut my_hashmap: HashMap<&str, i32> = HashMap::new();
+    my_hashmap.insert("CBS", 2);
+    my_hashmap.insert("Intl", 3);
+    my_hashmap.insert("BBC", 4);
 
-    for number in my_iterable {
-        println!("{:?}", number);
+    for (ch, status) in &mut my_hashmap {
+        // the order is different each time
+        println!("Channel: {:?} Complete: {:?}", ch, status);
+        *status = 256;
     }
-    // println!("{:?}", my_iterable); // for loop, borrowed it.
-
-    println!("------------------------------------------------------------------",);
-
-    let my_vetor_2 = vec![5, 12, 500, 214, 5];
-
-    // but this will work too, why?
-    // because rust will turn it to iterable for for loop
-    for number in my_vetor_2 {
-        println!("{:?}", number);
-    }
-
-    // bool
-    // let my_vetor_bool: Vec<bool> = vec![true, false, true];
-    // let my_iterable_bool = my_vetor_bool.into_iter(); // make vector iterable
-
-    // //Hashmap
-    // let mut my_hashmap: HashMap<&str, i32> = HashMap::new();
-    // my_hashmap.insert("CBS", 2);
-    // my_hashmap.insert("Intl", 3);
-    // my_hashmap.insert("BBC", 4);
-
-    // println!("{:?}", my_hashmap);
-    // let my_iterable_hashmap: hash_map::IntoIter<&str, i32> = my_hashmap.into_iter(); // make vector iterable
-
-    // println!("{:?}", my_iterable_hashmap);
+    println!("Channels: {:?}", my_hashmap)
 }
