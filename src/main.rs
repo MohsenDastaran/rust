79512eeb3190f1ba14cd7bb463872caecc4afa9d
MohsenDastaran <mohsen.dastaran@gmail.com>
2026-07-24 06:09:58 +0330

    17. The filter and find Methods I

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-use std::{collections::HashSet, mem::replace};
-
 fn main() {
-    let names = [
-        String::from("Mohsen"),
-        String::from("Ali"),
-        String::from("Pouya"),
+    let nums = [
+        10, 54, 0, 1, 0, 212, 54, 3541, 25341, 321, 321321, 3212, 121, 547, 8564, 8785, 465, 4654,
+        65413, 241564, 251, 541,
     ];
 
-    let name_lengths = names
-        .iter()
-        .map(|name| name.to_lowercase())
-        .map(|name| name.replace("i", "@@"))
-        .map(|name| name.len())
-        .collect::<Vec<usize>>();
+    //filter
+    let even_nums: Vec<i32> = nums.iter().filter(|num| *num % 2 == 0).copied().collect();
+    println!("{:?}", even_nums);
 
-    println!("{:?}", name_lengths)
+    //find
+    let first_odd = nums.iter().find(|num| *num % 2 != 0).copied();
+    println!("{:?}", first_odd);
 }
